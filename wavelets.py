1620026a2548a75e20919a0eed6158bef3f55f4c
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2026-08-18 21:09:05 -0600

    Add files via upload

--- wavelets.py
+++ wavelets.py
@@ -1,19 +1,22 @@
 import streamlit as st
 from streamlit.components.v1 import html
 
-st.title('Seismic Wavelets v.4.1')
+st.title('Seismic Wavelets: Wedge Model v.4.1')
 st.subheader('Alex Falkovskiy')
 url = "https://www.rmseismic.com"
 st.write("RM Seismic Software [rmseismic.com](%s)" % url)
-st.write('The purpose of this web app is to illustrate how basic seismic wavelets change with the change of parameters, and calculate synthetic seisimc.')
-st.write('You can also covolve any of these wavelets with simple reflectivity series and calculate a synthetic trace.')
-st.write('Wavelet equations used in this app are from a paper by Harold Ryan: Ricker, Ormsby; Klauder, Butterworth - A Choice of wavelets, publised in CSEG Recorder, September 1994.')
+st.write('This web app is a tool which helps to visualise how classical wedge model would look like on a seismic section.')
+st.write('It convolves any of selected wavelets with reflectivity series and calculates a synthetic traces for a wedge model.')
+st.write('You can select wavelet and vary model parameters and see how your seismic section would change on the fly.')
+
 st.write('Here is the list of wavelets you can display and calculate a synthetic trace:')
 st.write('**Ricker, Ormsby, Klauder, Butterworth**. Use a left side menu to select a wavelet and sliders to change parameters.')
+st.write('Wavelet equations are from a paper by Harold Ryan: Ricker, Ormsby; Klauder, Butterworth - A Choice of wavelets, publised in CSEG Recorder, September 1994.')
+st.write('This classical work provides math foundation for the wedge model: Widess, M. B., 1973, How thin is a thin bed?: Geophysics, 38, 1176-1180.')
 
 url1 = "https://www.rmseismic.com/lasviewer.html"
 st.write("More geophysical apps: [rmseismic.com](%s)" % url1)
-st.write("A.F., Jan 30, 2026")
+st.write("A.F., August 2026")
 
 google_js = """
 <!-- Google tag (gtag.js) --><script async src="https://www.googletagmanager.com/gtag/js?id=G-VBX865DFKL"></script><script>
